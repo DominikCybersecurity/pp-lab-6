a3b96db77a0ea0557a1c8792384300dfc739a6ec
Dominik Sulima <166212630+DominikCybersecurity@users.noreply.github.com>
2024-05-05 13:10:51 +0200

    Main.java

--- Main.java
+++ Main.java
@@ -4,11 +4,11 @@ import company.abstracts.Employee;
 
 public class Main {
     public static void main(String[] args) {
-        Worker worker1 = new Worker("Dorian", 2500, 1);
-        Worker worker2 = new Worker("Robert", 3400, 2);
-        Worker worker3 = new Worker("Artur", 2100, 3);
-        Worker worker4 = new Worker("Tomasz", 3200, 4);
-        Manager manager = new Manager("Bonifacy", 3000, 5);
+        Worker worker1 = new Worker("Dorian", 2500, 1, "21-01-2023", "Ochroniarz");
+        Worker worker2 = new Worker("Robert", 3400, 2, "23-03-2023", "Sprzątacz");
+        Worker worker3 = new Worker("Artur", 2100, 3, "15-04-2023", "Dozorca");
+        Worker worker4 = new Worker("Tomasz", 3200, 4, "19-05-2023", "Recepcjonista");
+        Manager manager = new Manager("Bonifacy", 3000, 5, "25-03-2022", "Manager");
 
         Employee[] employees = {worker1, worker2, worker3, worker4, manager}; 
 
