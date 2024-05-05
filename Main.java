8f4a4bd1cdaee18a59e846fbacb0bd4a389a7a34
Dominik Sulima <166212630+DominikCybersecurity@users.noreply.github.com>
2024-05-05 11:39:53 +0200

    Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
         Worker worker1 = new Worker("Dorian", 2500, 1);
         Worker worker2 = new Worker("Robert", 3400, 2);
         Worker worker3 = new Worker("Artur", 2100, 3);
-        Worker worker4 = new Worker("TomaszMarcel", 3200, 4);
+        Worker worker4 = new Worker("Tomasz", 3200, 4);
         Manager manager = new Manager("Bonifacy", 3000, 5);
 
         Worker[] workers = {worker1, worker2, worker3, worker4};
