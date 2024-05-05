03af384861da430eab90cffb1551294c2de0eefe
Dominik Sulima <166212630+DominikCybersecurity@users.noreply.github.com>
2024-05-05 12:59:09 +0200

    Main.java

--- Main.java
+++ Main.java
@@ -6,11 +6,11 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Worker worker1 = new Worker("Robert", 5100, 1, "21-01-2023", "Kasjer");
-        Worker worker2 = new Worker("Eugeniusz", 4800, 2, "23-03-2023", "Sprzątaczka");
-        Worker worker3 = new Worker("Kacper", 3600, 3, "15-04-2023", "Ochroniarz");
-        Worker worker4 = new Worker("Jakub", 6900, 4, "19-05-2023", "Konserwator");
-        Manager manager = new Manager("Hubert", 5200, 5, "25-03-2022", "Manager");
+        Worker worker1 = new Worker("Dorian", 2500, 1);
+        Worker worker2 = new Worker("Robert", 3400, 2);
+        Worker worker3 = new Worker("Artur", 2100, 3);
+        Worker worker4 = new Worker("Tomasz", 3200, 4);
+        Manager manager = new Manager("Bonifacy", 3000, 5);
 
         List<Employee> employees = new ArrayList<>();
         employees.add(worker1);
